821bf5849cb6a8c9fc927bba321ba7795c9525ef
Corey Moore <coreyamoore@gmail.com>
2019-10-16 11:01:20 -0400

    .

--- academic-pages/side-topsticky-navigation.js
+++ academic-pages/side-topsticky-navigation.js
@@ -162,7 +162,7 @@ jQuery(document).ready(function($){
         
         // Once the sidebar is outside of the viewport
         if(!$('.sidebar').is(':in-viewport')){
-            if($bodyColHeight < window.scrollY){
+            if(window.scrollY > $bodyColHeight){
                 $(rfi).css({
                     'position': 'absolute',
                     'top': 200 + $('.body').height() - $(rfi).height() - $('.sidebar').height()
